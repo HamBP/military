69ef87aa9904aea35134bfa8d32d4a9b8a6fadbf
HamBP <sjy_9484@naver.com>
2020-01-26 07:33:28 +0000

    왜 이벤트가 작동 안 하는지 모르겠다.

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-var inArmy = new Date(2019, 1, 11, 14, 0, 0);
-var outArmy = new Date(2020, 8, 16, 8, 0, 0);
-var curTime = new Date();
+$(document).ready(function() {
+    var inArmy = new Date(2019, 1, 11, 14, 0, 0);
+    var outArmy = new Date(2020, 8, 16, 8, 0, 0);
+    var curTime = new Date();
 
-var goalPeriod = outArmy - inArmy;
-var curPeriod = curTime - inArmy;
+    var goalPeriod = outArmy - inArmy;
+    var curPeriod = curTime - inArmy;
 
-var percent = curPeriod / goalPeriod;
-$(percent).html(percent.toString());
+    var percentage = 100 * (curPeriod / goalPeriod);
+
+    $(percent).html(percentage.toString());
+
+    $(re-calculate).on("click", function (e) {
+        percentage = 100 * (curPeriod / goalPeriod);
+        $(percent).html(percentage.toString());
+    })
+
+    $(percent-bar).each(function (i) {
+        if(i < 10) {
+            $(this).css("background-color", "green");
+        }
+    })
+
+    console.log("아무말");
+})
