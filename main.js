d3d97f504471554e4a498774db6a08961472da1b
HamBP <sjy_9484@naver.com>
2020-01-26 06:52:20 +0000

    퍼센트 계산

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-$(percent).html("10%");
+var inArmy = new Date(2019, 1, 11, 14, 0, 0);
+var outArmy = new Date(2020, 8, 16, 8, 0, 0);
+var curTime = new Date();
+
+var goalPeriod = outArmy - inArmy;
+var curPeriod = curTime - inArmy;
+
+var percent = curPeriod / goalPeriod;
+$(percent).html(percent.toString());
